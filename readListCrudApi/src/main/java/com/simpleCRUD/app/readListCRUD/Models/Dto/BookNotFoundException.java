75fa5bc3ed297dc4025975092293a138d5106a50
Kiril Gatsev <109879070+KirilGatsev@users.noreply.github.com>
2023-05-28 20:00:36 +0300

    Delete readListCrudApi directory

--- readListCrudApi/src/main/java/com/simpleCRUD/app/readListCRUD/Models/Dto/BookNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.simpleCRUD.app.readListCRUD.Models.Dto;
-
-
-public class BookNotFoundException extends RuntimeException{
-    public BookNotFoundException(Long id){
-        super("Book with id: " + id + " not in reading list.");
-    }
-}
--- readListCrudApi/src/main/java/com/simpleCRUD/app/readListCRUD/Services/Status.java
+++
@@ -1,7 +0,0 @@
-package com.simpleCRUD.app.readListCRUD.Services;
-
-public enum Status {
-    COMPLETED,
-    CURRENTLY_READING,
-    DROPPED
-}
